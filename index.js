15e1c0b84c647bb494e8f8c30715e036aad95538
Harry <hrgus3@gmail.com>
2021-12-07 12:42:24 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  
+ // pull the first index(add it to a let?)
+//  let indexOne = array[0];
+//  console.log('first:', indexOne)
+ // iterate through the array from the last index inward(.forEach?)
+ for(let i = 0; i < array.length; i++){
+   const targetValue = target - array[i];
+   for (let j = i + 1; j < array.length; j++){
+      if (array[j] === targetValue){
+        return true
+      }
+    }
+    
+  }  return false
+ //  IF the SUM(i + j) is equal to the target, RETURN true
+
+ //  ELSE keep iterating
+
+ //  IF none of the pairs with the first index equal the target, iterate i to the next index (i++)
+
+ // then repeat the iteration of the last index (j--)
 }
+
+
 
 /* 
   Write the Big O time complexity of your function here
+  
 */
 
 /* 
   Add your pseudocode here
+    
 */
 
 /*
@@ -17,11 +42,6 @@ function hasTargetSum(array, target) {
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
-
-  // ([3, 8, 12, 4, 11, 7], 10)
-  //  3 + 7 = 10 returns true
-  // ([3, 8, 17, 4, 12, 7], 11)
-  // false
 
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
